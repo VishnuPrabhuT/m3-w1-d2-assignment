29524dab1dd47fcc04ce3f8b20c350c5b7c3d040
Vishnu <vprabh@pdisoftware.com>
2021-09-11 12:44:10 -0500

    done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const auth = require("http-auth");
+
+const basic = auth.basic({
+    file: path.join(__dirname, "../users.htpasswd")
+});
+
 const router = express.Router();
+
+const Registration = mongoose.model("Registration");
 
 const {
     check,
@@ -12,6 +22,19 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/registrations", basic.check((req, res) => {
+    Registration.find().then(registrations => {
+        res.render("index", {
+            title: "Listing Registrations",
+            registrations
+        });
+    }).catch(err => {
+        console.log(err);
+        res.send("Sorry! Something went wrong.");
+    });
+
+}));
+
 router.post("/", [
         check("name").isLength({
             min: 1
@@ -23,7 +46,15 @@ router.post("/", [
     (req, res) => {
         const errors = validationResult(req);
         if (errors.isEmpty()) {
-            res.send("Thank you for your registration!");
+
+            const registration = new Registration(req.body);
+            registration.save().then(() => {
+                res.send("Thank you for the registration!");
+            }).catch(err => {
+                console.log(err);
+                res.send("Sorry! Something went wrong.");
+            });
+
         } else {
             res.render("form", {
                 title: "Registration form",
